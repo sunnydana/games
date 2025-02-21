48e6e4cc46f67acdf16414a34cc425a4dfae788d
Dana <dana@Danas-MacBook-Pro.local>
2025-02-21 15:03:43 -0500

    Added conditionals

--- hello.c
+++ hello.c
@@ -1,10 +1,18 @@
 #include<stdio.h>
-int main(void){
+#include<string.h>
+int main(){
     printf("Hello Tony\n");
     char answer[100];
     printf("How's the gabagool?\n");
     scanf("%s", answer);
-    printf("Answer: %s\n", answer);
+    if(strcmp(answer, "bad") == 0){
+        printf("Whadaya gonna do");
+    }
+    else if(strcmp(answer, "good") == 0){
+        printf("Alright, so have another!");
+    }
     return 0;
 
 }
+
+/* printf("Answer: %s\n", answer); */
