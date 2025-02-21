9428d8191cdf8b3820d002ddb0ca1ebd491a3344
Dana <dana@Danas-MacBook-Pro.local>
2025-02-21 13:46:58 -0500

    Added user input

--- hello.c
+++ hello.c
@@ -1,4 +1,10 @@
 #include<stdio.h>
 int main(void){
-    printf("Hello Tony");
+    printf("Hello Tony\n");
+    char answer[100];
+    printf("How's the gabagool?\n");
+    scanf("%s", answer);
+    printf("Answer: %s\n", answer);
+    return 0;
+
 }
