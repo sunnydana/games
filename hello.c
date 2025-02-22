f564e5a1b365a8c231dd26682ad4bd53f3accb1c
Dana <dana@danas-mbp.mynetworksettings.com>
2025-02-22 17:23:31 -0500

    Fixed the name

--- hello.c
+++ hello.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<string.h>
 int main(){
-    printf("Hello vietenammmm\n");
+    printf("Hello Vietnam\n");
     char answer[100];
     printf("How's the gabagool?\n");
     scanf("%s", answer);
