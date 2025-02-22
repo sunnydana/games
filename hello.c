c8e1fd7541b946f0ee1a207a25773b765c3eceef
Dana <dana@danas-mbp.mynetworksettings.com>
2025-02-22 16:50:33 -0500

    Changed Tony to Al

--- hello.c
+++ hello.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<string.h>
 int main(){
-    printf("Hello Tony\n");
+    printf("Hello Al\n");
     char answer[100];
     printf("How's the gabagool?\n");
     scanf("%s", answer);
