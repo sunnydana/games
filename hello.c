3c24424d81b82fe0c739838a3a39a8685278caf7
Dana <dana@danas-mbp.mynetworksettings.com>
2025-03-06 15:15:14 -0500

    Guess the number remade

--- hello.c
+++ hello.c
@@ -2,50 +2,23 @@
 #include<string.h>
 #include<stdlib.h>
 #include<time.h>
+
+
 int main(){
-    printf("Hello Vietnam\n");
-    char answer[100];
-    printf("How's the gabagool?\n");
-    scanf("%s", answer);
-    printf("Answer: %s\n", answer);
-   
-   srand(time(0)); 
-   int randomNumber = rand();
-   char numberAsk[10];
-   printf("Type 'number' for random number\n");
-   scanf("%s", numberAsk);
-   if (strcmp(numberAsk, "number" ) == 0){
-    printf("%d\n", randomNumber);
-   }
 
+    srand(time(0));
 
-    int number;
-    while(1){
-    printf("Enter a number:\n");
-    scanf("%d", &number);
-    if (number < 0){
-        printf("The number is negative\n");
-    }
-    else if (number > 0){
-        printf("The number is positive\n");
+    int number = rand();
+    int numberGuess;
+
+    printf("Hello! Welcome to Gabagool World\n");
+    printf("Enter a number between 1 to 100\n");
+    scanf("%d", &numberGuess);
+    if (numberGuess == number){
+        printf("Congratulations! You Win!");
     }
     else {
-        printf("The number is neither positive or negative\n");
-        break;
+        printf("Incorrect! Try again Punk\n");
     }
-    }
-
-    int numberGuess;
-    while(1){
-        printf("Guess the number\n");
-        scanf("%d", &numberGuess);
-        if (numberGuess == 9){
-            printf("Correct!");
-            break;
-        }
-    }
-
-    return 0;
-
 
 }
