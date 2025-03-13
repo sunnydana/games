9d0f9ae3bdaaf3126d4ebbf2b0c836ce94350c5c
Dana <dana@danas-mbp.mynetworksettings.com>
2025-03-13 16:31:36 -0400

    changed guess from  1-100 to 1-10

--- hello.c
+++ hello.c
@@ -7,13 +7,13 @@ int main(){
 
     srand(time(0));
 
-    int number = (rand() % 100) + 1; 
+    int number = (rand() % 10) + 1; 
     int numberGuess;
 
     printf("Hello! Welcome to Gabagool World\n");
 
     while(1){
-      printf("Enter a number between 1 to 100\n");
+      printf("Enter a number between 1 to 10\n");
 
       if (scanf("%d", &numberGuess) != 1) {
         printf("Number guesses only!\n");
