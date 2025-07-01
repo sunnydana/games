c9d60c49f086bc30e6c6aa5bec557af51fd85ef8
Dana Tsutieva <dana@mac.mynetworksettings.com>
2025-07-01 14:15:39 -0400

    completed Task 6

--- StructsPart2Task6.c
+++ StructsPart2Task6.c
@@ -10,6 +10,9 @@ int main(){
     printf("Hogwarts House Points struct\n");
     struct House theHouses[4];
 
+    int highestPoints = 0;
+    char highestPointsHouse[50];
+
     strcpy(theHouses[0].name, "Gryffindor");
     strcpy(theHouses[1].name, "Hufflepuff");
     strcpy(theHouses[2].name, "Ravenclaw");
@@ -18,6 +21,12 @@ int main(){
     for(int i = 0; i < 4; i++){
         printf("Enter the current points for house %s:\n", theHouses[i].name);
         scanf("%d", &theHouses[i].points);
+
+        if(theHouses[i].points > highestPoints){
+            highestPoints = theHouses[i].points;
+            strcpy(highestPointsHouse, theHouses[i].name);
+        }
+
     }
 
     printf("\n");
@@ -26,5 +35,63 @@ int main(){
         printf("%s house points: %d\n", theHouses[j].name, theHouses[j].points);
     }
 
+    printf("The house with the highest points is %s!", highestPointsHouse);
+
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+struct House {
+    char name[500];
+    int points;
+};
+
+int main(){
+    printf("Hogwarts House Points struct\n");
+    struct House theHouses[4];
+
+    strcpy(theHouses[0].name, "Gryffindor");
+    strcpy(theHouses[1].name, "Hufflepuff");
+    strcpy(theHouses[2].name, "Ravenclaw");
+    strcpy(theHouses[3].name, "Slytherin");
+
+    int highestPoints = 0;
+    char highestPointsHouse[50];
+
+    for(int i = 0; i < 4; i++){
+        printf("Enter the current points for house %s:\n", theHouses[i].name);
+        scanf("%d", &theHouses[i].points);
+
+        if(theHouses[i].points > 0){
+            highestPoints = theHouses[i].points;
+            highestPointsHouse = theHouses[i].name;
+        }
+
+    }
+
+    printf("\n");
+
+    for(int j = 0; j < 4; j++){
+        printf("%s house points: %d\n", theHouses[j].name, theHouses[j].points);
+    }
+
+    
+    printf("The house with the highest points is %s ", highestPointsHouse);
+
+    
+    
+
+
+}
+*/
